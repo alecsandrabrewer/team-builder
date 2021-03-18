4dd47e8d10eae2ef8e432f3887b7c020025b8892
Alecsandra Brewer <alecsandrabrewer@yahoo.com>
2021-03-17 20:24:44 -0700

    created list

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const teamMembersList = [
+    {Name: 'Allie', Email: 'ga.allie@yahoo.com', Role: 'Housewife'},
+    {Name: 'Ryan', Email: 'rl.brewer@yahoo.com', Role: 'Husband'},
+    {Name: 'Alfred', Email: 'princealfred@yahoo.com', Role: 'Crown Prince'},
+    {Name: 'Lucy', Email: 'whatsthat@yahoo.com', Role: 'Baby Girl'}
+  ];
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
